697c56142200e844dcc58c514414f42ceb4bb41e
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-27 21:27:46 +0000

    added polyfills for performance.now and requestAnimationFrame

---
+++ assets/js/utils/polyfills.js
@@ -0,0 +1,57 @@
+//#region performance.now()
+
+const moduleLoadTime = process && process.hrtime ? process.hrtime.bigint() : Date.now();
+
+const now = () => {
+  if (performance && performance.now) return performance.now();
+  else if (process && process.hrtime)
+    return (Number(process.hrtime.bigint()) - moduleLoadTime) / 1e6;
+
+  return Date.now() - moduleLoadTime;
+};
+
+//#endregion performance.now()
+
+//#region requestAnimationFrame()
+
+const QUEUE = [];
+
+let id = 0;
+let last = 0;
+
+function requestFixedTick(cb, fps = 60) {
+  if (QUEUE.length === 0) {
+    const frame = now();
+    const next = Math.max(0, 1000 / fps - (frame - last));
+    last = frame + next;
+
+    setTimeout(() => {
+      const stack = QUEUE.slice(0);
+      QUEUE.length = 0;
+      for (let i = 0; i < stack.length; i++) {
+        if (!stack[i].cancelled) {
+          stack[i].callback(last);
+        }
+      }
+    }, next);
+  }
+
+  QUEUE.push({
+    handle: ++id,
+    cancelled: false,
+    callback: cb,
+  });
+  return id;
+}
+
+function raf(cb, fps = -1) {
+  if (fps < 0 && requestAnimationFrame) return requestAnimationFrame(cb);
+  return requestFixedTick(cb, fps);
+}
+
+function caf(id) {
+  if (cancelAnimationFrame) cancelAnimationFrame(id);
+  for (let i = 0; i < QUEUE.length; i++) if (QUEUE[i].handle === id) QUEUE[i].cancelled = true;
+}
+
+//#endregion requestAnimationFrame()
