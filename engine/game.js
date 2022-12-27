b8b91cf52ecbebc5aa45dc29da5dd43243e504e7
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-27 04:09:22 +0000

    added function to generate canvas

--- engine/game.js
+++ engine/game.js
@@ -1,11 +1,8 @@
 class Game {
   constructor() {
-    this.viewport = document.createElement("canvas");
+    this.viewport = generateCanvas(500, 500);
     this.ctx = this.viewport.getContext("2d");
-    this.viewport.width = 800;
-    this.viewport.height = 600;
     document.body.insertBefore(this.viewport, document.body.firstChild);
-    // Toss some text into our canvas
     this.ctx.font = "32px Arial";
     this.ctx.fillText("We have begun.", 5, 50, 800);
   }
--- engine/utils.js
+++ engine/utils.js
@@ -15,3 +15,21 @@ function getPixelRatio(context) {
   // Get the proper pixel ratio by dividing the device ratio by the backing ratio.
   return deviceRatio / backingRatio;
 }
+
+function generateCanvas(w, h) {
+  console.log("Generating canvas.");
+  const canvas = document.createElement("canvas"),
+    ctx = canvas.getContext("2d");
+  const ratio = getPixelRatio(ctx);
+
+  // Set the canvas' width then downscale via CSS.
+  canvas.width = Math.round(w * ratio);
+  canvas.height = Math.round(h * ratio);
+  canvas.style.width = w + "px";
+  canvas.style.height = h + "px";
+
+  // Scale the context so we get accurate pixel density.
+  ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
+
+  return canvas;
+}
