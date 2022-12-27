cb1f85627aab75d3b940196e72a510992ff96126
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-27 06:03:35 +0000

    adding basic constants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,21 @@
 class Game {
-  constructor() {
-    this.viewport = generateCanvas(500, 500);
+  constructor(w, h, targetFps, showFps) {
+    this.constants = {
+      w,
+      h,
+      targetFps,
+      showFps,
+    };
+    this.state = {};
+    this.viewport = generateCanvas(w, h);
     this.ctx = this.viewport.getContext("2d");
+
+    //#region Remove later
     document.body.insertBefore(this.viewport, document.body.firstChild);
     this.ctx.font = "32px Arial";
     this.ctx.fillText("We have begun.", 5, 50, 800);
+    //#endregion Remove later
   }
 }
 
-window.game = new Game();
+window.game = new Game(800, 600, 60, true);
