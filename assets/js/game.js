436e71889450df469498585b2ebd88afc535b40a
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-27 06:16:08 +0000

    added render and update methods

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,14 +7,37 @@ class Game {
       showFps,
     };
     this.state = {};
+
     this.viewport = generateCanvas(w, h);
     this.ctx = this.viewport.getContext("2d");
-
-    //#region Remove later
     document.body.insertBefore(this.viewport, document.body.firstChild);
+  }
+
+  render() {
+    const { w, h } = this.constants;
+    this.ctx.clearRect(0, 0, w, h);
+
     this.ctx.font = "32px Arial";
-    this.ctx.fillText("We have begun.", 5, 50, 800);
-    //#endregion Remove later
+    this.ctx.fillStyle = "#fff";
+    this.ctx.fillText("It's dangerous to travel this route alone.", 5, 50);
+
+    // Displaying FPS.
+    if (this.constants.showFps) {
+      this.ctx.fillStyle = "#ff0";
+      this.ctx.fillText("FPS", w - 100, 50);
+    }
+
+    if (this.state.hasOwnProperty("entities")) {
+      const entities = scope.state.entities;
+      for (let entity in entities) entities[entity].render();
+    }
+  }
+
+  update(tFrame) {
+    if (this.state.hasOwnProperty("entities")) {
+      const entities = this.state.entities;
+      for (let entity in entities) entities[entity].update();
+    }
   }
 }
 
