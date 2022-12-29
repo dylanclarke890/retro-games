b4e1d0d10d525e5235c03f75b683017b140a0eec
dylanclarke890 <dylanclarke890@gmail.com>
2022-12-29 06:25:18 +0000

    tidy up

--- assets/js/utils/native-object-extensions.js
+++ assets/js/utils/native-object-extensions.js
@@ -33,11 +33,11 @@ Number.prototype.toDeg = function () {
 
 Object.defineProperty(Array.prototype, "erase", {
   value: function (item) {
-    for (let i = this.length; i--; ) {
+    for (let i = this.length; i--; )
       if (this[i] === item) {
         this.splice(i, 1);
+        break;
       }
-    }
     return this;
   },
 });
