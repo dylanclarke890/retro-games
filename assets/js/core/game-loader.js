f8e3a17945bb68e0cceb408e0239c2de54e2fb90
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-02 18:24:31 +0000

    tidying up game loader

--- assets/js/core/game-loader.js
+++ assets/js/core/game-loader.js
@@ -1,52 +1,50 @@
 class GameLoader {
-  gameClass = null;
-  status = 0;
   done = false;
-
-  #drawStatus = 0;
-  #resources = [];
-  #unloaded = [];
+  #gameClass = null;
+  #runner = null;
+  #status = 0;
+  #progressPercent = 0;
   #intervalId = 0;
+  #unloaded = [];
+  #resources = [];
 
-  constructor({ runner, system, gameClass, resources }) {
+  constructor({ runner, gameClass, resources }) {
     if (!runner) throw new Error("Runner is required.");
-    if (!system) throw new Error("System is required.");
     if (!gameClass) throw new Error("Game class is required.");
     if (!(gameClass.prototype instanceof Game))
-      throw new Error("Please pass a class that has derived from the 'Game' class.");
+      throw new Error("Please pass a class that has extended the 'Game' class.");
 
-    this.runner = runner;
-    this.system = system;
-    this.gameClass = gameClass;
+    this.#runner = runner;
+    this.#gameClass = gameClass;
     this.#resources = resources ?? [];
     for (let i = 0; i < this.#resources.length; i++) this.#unloaded.push(this.#resources[i].path);
   }
 
   load() {
-    this.system.clear("#000");
+    this.#runner.system.clear("#000");
     if (!this.#resources.length) {
-      this.end();
+      this.#end();
       return;
     }
-    for (let i = 0; i < this.#resources.length; i++) this.loadResource(this.#resources[i]);
-    this.#intervalId = setInterval(() => this.drawLoadingScreen(), 16);
+    for (let i = 0; i < this.#resources.length; i++) this.#loadResource(this.#resources[i]);
+    this.#intervalId = setInterval(() => this.#drawLoadingScreen(), 16);
   }
 
-  loadResource(res) {
-    res.load((path, status) => this.#loadCallback(path, status));
+  #loadResource(resource) {
+    resource.load((path, success) => this.#loadCallback(path, success));
   }
 
-  end() {
+  #end() {
     if (this.done) return;
     this.done = true;
     clearInterval(this.#intervalId);
-    this.runner.setGame(this.gameClass);
+    this.#runner.setGame(this.#gameClass);
   }
 
-  // TODO: Check this.
-  drawLoadingScreen() {
-    this.#drawStatus += (this.status - this.#drawStatus) / 5;
-    const system = this.system;
+  // FIXME: Progress bar is off center.
+  #drawLoadingScreen() {
+    this.#progressPercent += (this.#status - this.#progressPercent) / 5;
+    const system = this.#runner.system;
     const scale = system.scale;
     let width = (system.width * 0.6).floor();
     let height = (system.height * 0.1).floor();
@@ -62,13 +60,13 @@ class GameLoader {
     system.ctx.fillStyle = "#000";
     system.ctx.fillRect(x + scale, y + scale, width - scale - scale, height - scale - scale);
     system.ctx.fillStyle = "#fff";
-    system.ctx.fillRect(x, y, width * this.#drawStatus, height);
+    system.ctx.fillRect(x, y, width * this.#progressPercent, height);
   }
 
-  #loadCallback(path, status) {
-    if (!status) throw new Error(`Failed to load resource: ${path}`);
+  #loadCallback(path, wasSuccessful) {
+    if (!wasSuccessful) throw new Error(`Failed to load resource: ${path}`);
     this.#unloaded.erase(path);
-    this.status = 1 - this.#unloaded.length / this.#resources.length;
-    if (this.#unloaded.length == 0) setTimeout(() => this.end(), 250); // all done?
+    this.#status = 1 - this.#unloaded.length / this.#resources.length;
+    if (this.#unloaded.length === 0) setTimeout(() => this.#end(), 250);
   }
 }
