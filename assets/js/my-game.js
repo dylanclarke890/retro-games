3f158d941fa16dd7219e27b6b1f44f076890cdb0
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-02 18:05:00 +0000

    removing commented code

--- assets/js/my-game.js
+++ assets/js/my-game.js
@@ -29,14 +29,3 @@ const runner = new GameRunner({
     path: "./assets/fonts/arcade-classic.TTF",
   },
 });
-
-// ig.main = function (canvasId, gameClass, fps, width, height, scale, loaderClass) {
-//   ig.system = new ig.System(canvasId, fps, width, height, scale || 1);
-//   ig.input = new ig.Input();
-//   ig.soundManager = new ig.SoundManager();
-//   ig.music = new ig.Music();
-//   ig.ready = true;
-
-//   var loader = new (loaderClass || ig.Loader)(gameClass, ig.resources);
-//   loader.load();
-// };
