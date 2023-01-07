7c4387fae29a9b64d26dd83ed18c30609c68d59c
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-07 18:08:59 +0000

    added .d.js file for better intellisense

---
+++ game-engine.d.js
@@ -0,0 +1,34 @@
+/// /// <reference path="assets/js/my-game.js" />
+// #region DEBUG
+/// <reference path="assets/js/debug/performance.js"/>
+// #endregion DEBUG
+
+// #region LIB
+/// <reference path="assets/js/lib/polyfills.js"/>
+/// <reference path="assets/js/lib/native-object-extensions.js"/>
+/// <reference path="assets/js/lib/guard.js"/>
+/// <reference path="assets/js/lib/register.js"/>
+/// <reference path="assets/js/lib/vendor-attributes.js"/>
+/// <reference path="assets/js/lib/user-agent.js"/>
+/// <reference path="assets/js/lib/timer.js"/>
+/// <reference path="assets/js/lib/input.js"/>
+/// <reference path="assets/js/lib/image.js"/>
+/// <reference path="assets/js/lib/font.js"/>
+/// <reference path="assets/js/lib/animation.js"/>
+/// <reference path="assets/js/lib/sound.js"/>
+/// <reference path="assets/js/lib/map.js"/>
+/// <reference path="assets/js/lib/media-factory.js"/>
+// #endregion LIB
+
+// #region CORE
+/// <reference path="assets/js/core/game-loop.js"/>
+/// <reference path="assets/js/core/entity.js"/>
+/// <reference path="assets/js/core/system.js"/>
+/// <reference path="assets/js/core/game.js"/>
+/// <reference path="assets/js/core/runner.js"/>
+/// <reference path="assets/js/core/game-loader.js"/>
+// #endregion CORE
+
+/// <reference path="assets/js/pong-entities.js"/>
+/// <reference path="assets/js/level.js"/>
+/// <reference path="assets/js/my-game.js"/>
