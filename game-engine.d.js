2c58c59878bc3801f314b3aae6ae543e2686ef89
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-11 21:34:23 +0000

    moving files

--- game-engine.d.js
+++ game-engine.d.js
@@ -6,12 +6,10 @@
 /// <reference path="assets/js/lib/vendor-attributes.js"/>
 /// <reference path="assets/js/lib/user-agent.js"/>
 /// <reference path="assets/js/lib/timer.js"/>
-/// <reference path="assets/js/lib/input.js"/>
 /// <reference path="assets/js/lib/image.js"/>
 /// <reference path="assets/js/lib/font.js"/>
 /// <reference path="assets/js/lib/animation.js"/>
 /// <reference path="assets/js/lib/sound.js"/>
-/// <reference path="assets/js/lib/map.js"/>
 /// <reference path="assets/js/lib/media-factory.js"/>
 // #endregion LIB
 
@@ -19,8 +17,10 @@
 /// <reference path="assets/js/core/debug.js"/>
 /// <reference path="assets/js/core/entity.js"/>
 /// <reference path="assets/js/core/game.js"/>
+/// <reference path="assets/js/core/input.js"/>
 /// <reference path="assets/js/core/loader.js"/>
 /// <reference path="assets/js/core/loop.js"/>
+/// <reference path="assets/js/core/map.js"/>
 /// <reference path="assets/js/core/runner.js"/>
 /// <reference path="assets/js/core/system.js"/>
 // #endregion CORE
