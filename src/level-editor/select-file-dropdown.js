12026e82e1091d2bf0493aa4471ae13b77be4f03
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-14 19:04:52 +0000

    tidied up select file dropdown

--- src/level-editor/select-file-dropdown.js
+++ src/level-editor/select-file-dropdown.js
@@ -1,97 +1,99 @@
-ig.module("weltmeister.select-file-dropdown").defines(function () {
-  "use strict";
+class SelectFileDropdown {
+  /** @type {HTMLElement} */
+  div = null;
+  /** @type {HTMLElement} */
+  input = null;
+  boundShow = null;
+  filelistPHP = "";
+  filetype = "";
 
-  wm.SelectFileDropdown = ig.Class.extend({
-    input: null,
-    boundShow: null,
-    boundHide: null,
-    div: null,
-    filelistPHP: "",
-    filetype: "",
+  constructor(elementId, filelistPHP, filetype) {
+    this.filetype = filetype || "";
+    this.filelistPHP = filelistPHP;
+    this.input = $el(elementId);
+    this.input.addEventListener("focus", (e) => this.show(e));
+    this.div = $new("div");
+    this.div.classList.add("selectFileDialog");
+    this.div.addEventListener("mousedown", (e) => this.noHide(e));
 
-    init: function (elementId, filelistPHP, filetype) {
-      this.filetype = filetype || "";
-      this.filelistPHP = filelistPHP;
-      this.input = $(elementId);
-      this.boundHide = this.hide.bind(this);
-      this.input.bind("focus", this.show.bind(this));
+    this.input.after(this.div);
+    this.loadDir("");
+  }
 
-      this.div = $("<div/>", { class: "selectFileDialog" });
-      this.input.after(this.div);
-      this.div.bind("mousedown", this.noHide.bind(this));
+  loadDir(dir) {
+    const path =
+      this.filelistPHP + "?dir=" + encodeURIComponent(dir || "") + "&type=" + this.filetype;
+    // TODO
+    $.ajax({
+      url: path,
+      dataType: "json",
+      async: false,
+      success: this.showFiles.bind(this),
+    });
+  }
 
-      this.loadDir("");
-    },
+  selectDir(event) {
+    this.loadDir(event.target.href);
+    return false;
+  }
 
-    loadDir: function (dir) {
-      var path =
-        this.filelistPHP + "?dir=" + encodeURIComponent(dir || "") + "&type=" + this.filetype;
-      var req = $.ajax({
-        url: path,
-        dataType: "json",
-        async: false,
-        success: this.showFiles.bind(this),
-      });
-    },
+  selectFile(event) {
+    this.input.value = event.target.href;
+    this.input.blur();
+    this.hide();
+    return false;
+  }
 
-    selectDir: function (event) {
-      this.loadDir($(event.target).attr("href"));
-      return false;
-    },
+  showFiles(data) {
+    this.div.innerHTML = "";
+    if (data.parent !== false) {
+      const parent = $new("a");
+      parent.classList.add("dir");
+      parent.href = data.parent;
+      parent.innerHTML = "&hellip;parent directory";
+      parent.addEventListener("click", (e) => this.selectDir(e));
+      this.div.append(parentDir);
+    }
+    for (let i = 0; i < data.dirs.length; i++) {
+      const name = data.dirs[i].match(/[^\/]*$/)[0] + "/";
+      const dir = $new("a");
+      dir.classList.add("dir");
+      dir.href = data.dirs[i];
+      dir.innerHTML = name;
+      dir.title = name;
+      dir.addEventListener("click", (e) => this.selectDir(e));
+      this.div.append(dir);
+    }
+    for (let i = 0; i < data.files.length; i++) {
+      const name = data.files[i].match(/[^\/]*$/)[0];
+      const file = $new("a");
+      file.classList.add("file");
+      file.href = data.files[i];
+      file.innerHTML = name;
+      file.title = name;
+      file.addEventListener("click", (e) => this.selectFile(e));
+      this.div.append(file);
+    }
+  }
 
-    selectFile: function (event) {
-      this.input.val($(event.target).attr("href"));
-      this.input.blur();
-      this.hide();
-      return false;
-    },
+  noHide(event) {
+    event.stopPropagation();
+  }
 
-    showFiles: function (data) {
-      this.div.empty();
-      if (data.parent !== false) {
-        var parentDir = $("<a/>", {
-          class: "dir",
-          href: data.parent,
-          html: "&hellip;parent directory",
-        });
-        parentDir.bind("click", this.selectDir.bind(this));
-        this.div.append(parentDir);
-      }
-      for (var i = 0; i < data.dirs.length; i++) {
-        var name = data.dirs[i].match(/[^\/]*$/)[0] + "/";
-        var dir = $("<a/>", { class: "dir", href: data.dirs[i], html: name, title: name });
-        dir.bind("click", this.selectDir.bind(this));
-        this.div.append(dir);
-      }
-      for (var i = 0; i < data.files.length; i++) {
-        var name = data.files[i].match(/[^\/]*$/)[0];
-        var file = $("<a/>", { class: "file", href: data.files[i], html: name, title: name });
-        file.bind("click", this.selectFile.bind(this));
-        this.div.append(file);
-      }
-    },
+  show(_event) {
+    const top = this.input.offsetTop;
+    const left = this.input.offsetLeft;
+    const inputHeight = getInnerHeight(this.input) + parseInt(this.input.style.marginTop);
+    const inputWidth = getInnerWidth(this.input);
+    document.addEventListener("mousedown", () => this.hide());
+    this.div.top = `${top + inputHeight + 1}px`;
+    this.div.left = `${left}px`;
+    this.div.width = `${inputWidth}px`;
+    slideDown(this.div, 100);
+  }
 
-    noHide: function (event) {
-      event.stopPropagation();
-    },
-
-    show: function (event) {
-      var inputPos = this.input.position(); //this.input.getPosition(this.input.getOffsetParent());
-      var inputHeight = parseInt(this.input.innerHeight()) + parseInt(this.input.css("margin-top"));
-      var inputWidth = this.input.innerWidth();
-      $(document).bind("mousedown", this.boundHide);
-      this.div
-        .css({
-          top: inputPos.top + inputHeight + 1,
-          left: inputPos.left,
-          width: inputWidth,
-        })
-        .slideDown(100);
-    },
-
-    hide: function () {
-      $(document).unbind("mousedown", this.boundHide);
-      this.div.slideUp(100);
-    },
-  });
-});
+  hide() {
+    document.removeEventListener("mousedown", () => this.hide());
+    slideUp(this.div, 100);
+  }
+}
