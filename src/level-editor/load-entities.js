32fd6bf27442f28ed7440bd15fca0ef921ca9589
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-16 21:50:55 +0000

    removed test file

--- src/level-editor/load-entities.js
+++ src/level-editor/load-entities.js
@@ -2,3 +2,17 @@ async function loadEntities(config) {
   const apiClient = new LevelEditorApiClient();
   return await apiClient.client.glob(config.project.entityFiles);
 }
+
+function loadEntityScripts(entityObj) {
+  Object.keys(entityObj).forEach((filepath) => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.addEventListener("load", (_e) => {
+      console.log(`Loaded:${filepath}`);
+    });
+    document.head.append(script);
+    script.src = filepath;
+  });
+}
+
+loadEntities(levelEditorConfig).then((val) => loadEntityScripts(val));
