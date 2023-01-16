fbde2f2006ee055f3dc91f03c007b6b739b96ecd
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-16 21:56:38 +0000

    refactoring load entities

--- src/level-editor/load-entities.js
+++ src/level-editor/load-entities.js
@@ -3,16 +3,18 @@ async function loadEntities(config) {
   return await apiClient.client.glob(config.project.entityFiles);
 }
 
+function loadScript({ src, cb = (_e, _path) => {} } = {}) {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.addEventListener("load", (e) => cb(e, src));
+  script.src = src;
+}
+
 function loadEntityScripts(entityObj) {
-  Object.keys(entityObj).forEach((filepath) => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.addEventListener("load", (_e) => {
-      console.log(`Loaded:${filepath}`);
-    });
-    document.head.append(script);
-    script.src = filepath;
-  });
+  const cb = (_e, filepath) => {
+    console.log(`Loaded:${filepath}`);
+  };
+  Object.keys(entityObj).forEach((filepath) => loadScript({ src: filepath, cb }));
 }
 
 loadEntities(levelEditorConfig).then((val) => loadEntityScripts(val));
