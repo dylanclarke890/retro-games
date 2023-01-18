38c2bd2d13f80205c40625552ec7d4a6388bbd7d
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-18 03:28:20 +0000

    added browse api

--- server/level-editor/browse.php
+++ server/level-editor/browse.php
@@ -1,3 +1,32 @@
 <?php
 
 declare(strict_types=1);
+
+$file_root = dirname(__FILE__, 3);
+$file_root_len = strlen($file_root);
+
+$dir_val = $_GET["dir"];
+$dir = $file_root . str_replace('..', '', $dir_val);
+if ($dir[strlen($dir) - 1] != '/') $dir .= '/';
+
+$find = '*.*';
+switch ($_GET['type']) {
+  case 'images':
+    $find = '*.{png,gif,jpg,jpeg}';
+    break;
+  case 'scripts':
+    $find = '*.js';
+    break;
+}
+
+$dirs = glob($dir . '*', GLOB_ONLYDIR) ?? array();
+foreach ($dirs as $i => $d) $dirs[$i] = substr($d, $file_root_len);
+$files = glob($dir . $find, GLOB_BRACE) ?? array();
+foreach ($files as $i => $f) $files[$i] = substr($f, $file_root_len);
+
+$parent = substr($dir_val, 0, strrpos($dir_val, '/'));
+echo json_encode(array(
+  'parent' => (empty($dir_val) ? false : $parent),
+  'dirs' => $dirs,
+  'files' => $files
+));
