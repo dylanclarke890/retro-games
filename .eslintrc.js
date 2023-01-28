d243913ccd64f61f80f8e8f9695d60cf5627d363
dylanclarke890 <dylanclarke890@gmail.com>
2023-01-28 20:48:14 +0000

    enabled eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
