a1b410b5a2f79cddf4bd80255cd29ddc21610977
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-02 11:08:20 +0000

    moved number utils to functions rather than extending the number prototype

---
+++ modules/lib/number-utils.js
@@ -0,0 +1,24 @@
+export function constrain(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
+export function map(value, istart, istop, ostart, ostop) {
+  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+}
+
+export function round(value, precision) {
+  precision = Math.pow(10, precision || 0);
+  return Math.round(value * precision) / precision;
+}
+
+export function toInt(value) {
+  return value | 0;
+}
+
+export function toRad(value) {
+  return (value / 180) * Math.PI;
+}
+
+export function toDeg(value) {
+  return (value * 180) / Math.PI;
+}
