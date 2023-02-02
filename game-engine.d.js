333514ed166858fae70520ba71988399197e9f4d
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-02 20:48:41 +0000

    organised d.js file

--- game-engine.d.js
+++ game-engine.d.js
@@ -1,14 +1,18 @@
 // #region LIB
-/// <reference path="modules/lib/polyfills.js"/>
-/// <reference path="modules/lib/native-object-extensions.js"/>
-/// <reference path="modules/lib/ui-effects.js"/>
+/// <reference path="modules/lib/bezier.js"/>
 /// <reference path="modules/lib/cookie.js"/>
+/// <reference path="modules/lib/event-chain.js"/>
 /// <reference path="modules/lib/guard.js"/>
-/// <reference path="modules/lib/vendor-attributes.js"/>
-/// <reference path="modules/lib/user-agent.js"/>
-/// <reference path="modules/lib/timer.js"/>
 /// <reference path="modules/lib/inject.js"/>
 /// <reference path="modules/lib/mixin.js"/>
+/// <reference path="modules/lib/native-object-extensions.js"/>
+/// <reference path="modules/lib/number-utils.js"/>
+/// <reference path="modules/lib/polyfills.js"/>
+/// <reference path="modules/lib/string-utils.js"/>
+/// <reference path="modules/lib/timer.js"/>
+/// <reference path="modules/lib/ui-effects.js"/>
+/// <reference path="modules/lib/user-agent.js"/>
+/// <reference path="modules/lib/vendor-attributes.js"/>
 // #endregion LIB
 
 // #region DEBUG
@@ -17,19 +21,19 @@
 // #endregion DEBUG
 
 // #region CORE
-/// <reference path="modules/core/register.js"/>
-/// <reference path="modules/core/image.js"/>
-/// <reference path="modules/core/font.js"/>
 /// <reference path="modules/core/animation.js"/>
-/// <reference path="modules/core/sound.js"/>
-/// <reference path="modules/core/media-factory.js"/>
 /// <reference path="modules/core/entity.js"/>
+/// <reference path="modules/core/font.js"/>
 /// <reference path="modules/core/game.js"/>
+/// <reference path="modules/core/image.js"/>
 /// <reference path="modules/core/input.js"/>
 /// <reference path="modules/core/loader.js"/>
 /// <reference path="modules/core/loop.js"/>
 /// <reference path="modules/core/map.js"/>
+/// <reference path="modules/core/media-factory.js"/>
+/// <reference path="modules/core/register.js"/>
 /// <reference path="modules/core/runner.js"/>
+/// <reference path="modules/core/sound.js"/>
 /// <reference path="modules/core/system.js"/>
 // #endregion CORE
 
@@ -40,6 +44,8 @@
 /// <reference path="modules/level-editor/edit-entities.js"/>
 /// <reference path="modules/level-editor/edit-map.js"/>
 /// <reference path="modules/level-editor/evented-input.js"/>
+/// <reference path="modules/level-editor/http-client.js"/>
+/// <reference path="modules/level-editor/level-editor-runner.js"/>
 /// <reference path="modules/level-editor/level-editor.js"/>
 /// <reference path="modules/level-editor/modal-dialogs.js"/>
 /// <reference path="modules/level-editor/select-file-dropdown.js"/>
@@ -48,10 +54,15 @@
 //#endregion LEVEL-EDITOR
 
 //#region PLUGINS
-/// <reference path="modules/plugins/retro-highscore-namefield.js"/>
+/// <reference path="modules/plugins/clear-map-tile.js"/>
+/// <reference path="modules/plugins/clickable.js"/>
+/// <reference path="modules/plugins/edges.js"/>
+/// <reference path="modules/plugins/fade-entity.js"/>
+/// <reference path="modules/plugins/fixed-entity.js"/>
 /// <reference path="modules/plugins/observable.js"/>
+/// <reference path="modules/plugins/retro-highscore-namefield.js"/>
+/// <reference path="modules/plugins/state-machine.js"/>
 //#endregion PLUGINS
-
 
 // #region CUSTOM
 /// <reference path="example/my-game.js"/>
