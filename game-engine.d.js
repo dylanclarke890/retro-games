d8bcc8eef3e3dcdcd25cb0b66f1ad975c037ca67
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-02 23:26:07 +0000

    organised d.js

--- game-engine.d.js
+++ game-engine.d.js
@@ -1,25 +1,3 @@
-// #region LIB
-/// <reference path="modules/lib/bezier.js"/>
-/// <reference path="modules/lib/cookie.js"/>
-/// <reference path="modules/lib/event-chain.js"/>
-/// <reference path="modules/lib/guard.js"/>
-/// <reference path="modules/lib/inject.js"/>
-/// <reference path="modules/lib/mixin.js"/>
-/// <reference path="modules/lib/native-object-extensions.js"/>
-/// <reference path="modules/lib/number-utils.js"/>
-/// <reference path="modules/lib/polyfills.js"/>
-/// <reference path="modules/lib/string-utils.js"/>
-/// <reference path="modules/lib/timer.js"/>
-/// <reference path="modules/lib/ui-effects.js"/>
-/// <reference path="modules/lib/user-agent.js"/>
-/// <reference path="modules/lib/vendor-attributes.js"/>
-// #endregion LIB
-
-// #region DEBUG
-/// <reference path="modules/debug/performance-stats.js"/>
-/// <reference path="modules/debug/debug.js"/>
-// #endregion DEBUG
-
 // #region CORE
 /// <reference path="modules/core/animation.js"/>
 /// <reference path="modules/core/entity.js"/>
@@ -37,6 +15,17 @@
 /// <reference path="modules/core/system.js"/>
 // #endregion CORE
 
+// #region DEBUG
+/// <reference path="modules/debug/performance-stats.js"/>
+/// <reference path="modules/debug/debug.js"/>
+// #endregion DEBUG
+
+// #region EXAMPLE
+/// <reference path="example/my-game.js"/>
+/// <reference path="example/pong-entities.js"/>
+/// <reference path="example/level.js"/>
+// #endregion EXAMPLE
+
 //#region LEVEL-EDITOR
 /// <reference path="modules/level-editor/jQuery/jquery-1.7.1.min.js"/>
 /// <reference path="modules/level-editor/jQuery/jquery-ui-1.8.1.custom.min.js"/>
@@ -53,9 +42,28 @@
 /// <reference path="modules/level-editor/undo.js"/>
 //#endregion LEVEL-EDITOR
 
+// #region LIB
+/// <reference path="modules/lib/bezier.js"/>
+/// <reference path="modules/lib/cookie.js"/>
+/// <reference path="modules/lib/event-chain.js"/>
+/// <reference path="modules/lib/guard.js"/>
+/// <reference path="modules/lib/inject.js"/>
+/// <reference path="modules/lib/mixin.js"/>
+/// <reference path="modules/lib/native-object-extensions.js"/>
+/// <reference path="modules/lib/number-utils.js"/>
+/// <reference path="modules/lib/polyfills.js"/>
+/// <reference path="modules/lib/string-utils.js"/>
+/// <reference path="modules/lib/timer.js"/>
+/// <reference path="modules/lib/ui-effects.js"/>
+/// <reference path="modules/lib/user-agent.js"/>
+/// <reference path="modules/lib/vendor-attributes.js"/>
+// #endregion LIB
+
 //#region PLUGINS
 /// <reference path="modules/plugins/clear-map-tile.js"/>
 /// <reference path="modules/plugins/clickable.js"/>
+/// <reference path="modules/plugins/combo-manager.js"/>
+/// <reference path="modules/plugins/distance-based-sound.js"/>
 /// <reference path="modules/plugins/edges.js"/>
 /// <reference path="modules/plugins/fade-entity.js"/>
 /// <reference path="modules/plugins/fixed-entity.js"/>
@@ -63,9 +71,3 @@
 /// <reference path="modules/plugins/retro-highscore-namefield.js"/>
 /// <reference path="modules/plugins/state-machine.js"/>
 //#endregion PLUGINS
-
-// #region CUSTOM
-/// <reference path="example/my-game.js"/>
-/// <reference path="example/pong-entities.js"/>
-/// <reference path="example/level.js"/>
-// #endregion CUSTOM
