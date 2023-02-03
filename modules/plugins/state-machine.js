8d1d59dd517562cb8959b4c042987d9e488db50b
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-03 22:20:39 +0000

    rewrote state machine plugin

--- modules/plugins/state-machine.js
+++ modules/plugins/state-machine.js
@@ -1,29 +1,44 @@
-export const StateMachinePlugin = {
-  ready() {
-    this.parent();
-    this.setState(this.stateMachine.initialState);
+export const StateMachinePlugin = [
+  {
+    name: "ready",
+    value: function () {
+      this.parent();
+      this.setState(this.stateMachine.initialState);
+    },
   },
 
-  setState(state) {
-    this._currentState = state;
-    this._currentUpdate = this["update_" + state];
-    if (typeof this["_on_" + state] === "function") this["on_" + state]();
+  {
+    name: "setState",
+    value: function (state) {
+      this._currentState = state;
+      this._currentUpdate = this["update_" + state];
+      if (typeof this["_on_" + state] === "function") this["on_" + state]();
+    },
   },
 
-  stateIs(state) {
-    return this._currentState === state;
+  {
+    name: "stateIs",
+    value: function (state) {
+      return this._currentState === state;
+    },
   },
 
-  transition(transition) {
-    const stateDefinition = this.stateMachine[this._currentState];
-    if (!stateDefinition) return;
-    const transitions = stateDefinition.on;
-    if (!transitions || !transitions[transition]) return;
-    this.setState(transitions[transition]);
+  {
+    name: "transition",
+    value: function (transition) {
+      const stateDefinition = this.stateMachine[this._currentState];
+      if (!stateDefinition) return;
+      const transitions = stateDefinition.on;
+      if (!transitions || !transitions[transition]) return;
+      this.setState(transitions[transition]);
+    },
   },
 
-  update() {
-    if (this._currentUpdate) this._currentUpdate(this.parent);
-    else this.parent();
+  {
+    name: "update",
+    value: function () {
+      if (this._currentUpdate) this._currentUpdate(this.parent);
+      else this.parent();
+    },
   },
-};
+];
