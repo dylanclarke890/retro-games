577480574a78f16b908207565d8265471aeb9f1c
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-05 20:41:33 +0000

    extended edges mixin to account for shapes

--- modules/plugins/edges.js
+++ modules/plugins/edges.js
@@ -1,28 +1,49 @@
 export const EdgesMixin = (superclass) =>
   class extends superclass {
+    /** @type {"rectangle" | "circle"} */
+    collisionShape = "rectangle";
+
     constructor(opts) {
       super(opts);
-      Object.defineProperties(this, {
-        left: {
-          get() {
-            return this.pos.x;
-          },
-        },
-        right: {
-          get() {
-            return this.pos.x + this.size.x;
-          },
-        },
-        top: {
-          get() {
-            return this.pos.y;
-          },
-        },
-        bottom: {
-          get() {
-            return this.pos.y + this.size.y;
-          },
-        },
-      });
+    }
+
+    get left() {
+      switch (this.collisionShape) {
+        case "rectangle":
+        default:
+          return this.pos.x;
+        case "circle":
+          return this.pos.x - this.size.r;
+      }
+    }
+
+    get right() {
+      switch (this.collisionShape) {
+        case "rectangle":
+        default:
+          return this.pos.x + this.size.x;
+        case "circle":
+          return this.pos.x + this.size.r;
+      }
+    }
+
+    get top() {
+      switch (this.collisionShape) {
+        case "rectangle":
+        default:
+          return this.pos.y - this.size.y;
+        case "circle":
+          return this.pos.y - this.r;
+      }
+    }
+
+    get bottom() {
+      switch (this.collisionShape) {
+        case "rectangle":
+        default:
+          return this.pos.y + this.size.y;
+        case "circle":
+          return this.pos.y + this.r;
+      }
     }
   };
