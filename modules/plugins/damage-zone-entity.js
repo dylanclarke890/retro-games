f533838e43793ea5c781d0a4bcf3f659def01c66
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-05 19:47:33 +0000

    added damage zone entity

--- modules/plugins/damage-zone-entity.js
+++ modules/plugins/damage-zone-entity.js
@@ -1,65 +1,48 @@
-ig.module("game.entities.damagezone")
-  .requires("impact.entity")
-  .defines(function () {
-    EntityDamagezone = ig.Entity.extend({
-      _wmDrawBox: true,
-      _wmBoxColor: "rgba(0, 0, 255, 0.7)",
-      size: { x: 16, y: 16 },
-      damage: 9999,
-      ignoreknockback: "true",
+import { Entity } from "../core/entity.js";
 
-      checkAgainst: ig.Entity.TYPE.BOTH,
-      collides: ig.Entity.COLLIDES.ACTIVE,
+export class EntityDamageZone extends Entity {
+  _wmDrawBox = true;
+  _wmBoxColor = "rgba(0, 0, 255, 0.7)";
+  size = { x: 16, y: 16 };
+  damage = 9999;
+  ignoreknockback = "true";
 
-      init: function (x, y, settings) {
-        this.parent(x, y, settings);
-        if (settings.damage) {
-          this.damage = settings.damage;
-          this.ignoreknockback = this.stringToBoolean(settings.ignoreknockback);
-        }
-      },
+  checkAgainst = Entity.TYPE.BOTH;
+  collides = Entity.COLLIDES.ACTIVE;
 
-      update: function () {},
+  constructor({ x, y, game, settings }) {
+    super({ x, y, game, settings });
+    if (this.damage) this.ignoreknockback = this.stringToBoolean(this.ignoreknockback);
+  }
 
-      check: function (other) {
-        if (other instanceof EntityPlayer) {
-          other.receiveDamage(this.damage);
+  update() {}
 
-          if (!this.ignoreknockback) {
-            this.knockBack(other);
-          }
-        }
-      },
+  check(other) {
+    other.receiveDamage(this.damage);
+    if (!this.ignoreknockback) this.knockBack(other);
+  }
 
-      knockBack: function (other) {
-        if (other.pos.x < this.pos.x) {
-          other.vel.x = -other.maxVel.x;
-        } else if (other.pos.x > this.pos.x) {
-          other.vel.x = other.maxVel.x;
-        }
+  knockBack(other) {
+    if (other.pos.x < this.pos.x) other.vel.x = -other.maxVel.x;
+    else if (other.pos.x > this.pos.x) other.vel.x = other.maxVel.x;
 
-        if (other.pos.y < this.pos.y) {
-          other.vel.y = -other.maxVel.y;
-        } else if (other.pos.y > this.pos.y) {
-          other.vel.y = other.maxVel.y;
-        }
-      },
+    if (other.pos.y < this.pos.y) other.vel.y = -other.maxVel.y;
+    else if (other.pos.y > this.pos.y) other.vel.y = other.maxVel.y;
+  }
 
-      stringToBoolean: function (string) {
-        if (string != null) {
-          switch (string.toLowerCase()) {
-            case "true":
-            case "yes":
-            case "1":
-              return true;
-            case "false":
-            case "no":
-            case "0":
-              return false;
-            default:
-              return Boolean(string);
-          }
-        }
-      },
-    });
-  });
+  stringToBoolean(string) {
+    if (string == null) return false;
+    switch (string.toLowerCase()) {
+      case "true":
+      case "yes":
+      case "1":
+        return true;
+      case "false":
+      case "no":
+      case "0":
+        return false;
+      default:
+        return Boolean(string);
+    }
+  }
+}
